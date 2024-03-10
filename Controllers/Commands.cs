978b43013a42a071687ee645055d7f32d76ebfc2
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-10 11:59:08 +0800

    ss

--- Controllers/Commands.cs
+++ Controllers/Commands.cs
@@ -1,7 +1,8 @@
 using System;
+using System.Text;
 using TextAdventure;
 
-namespace User_Commands
+namespace User
 {
     class User_Command
     {
@@ -19,4 +20,59 @@ namespace User_Commands
             }
         }
     }
+}
+
+namespace Command
+{
+    class Command
+    {
+    public static string CustomConsoleInput(string prompt)
+    {
+        StringBuilder inputBuilder = new StringBuilder();
+        Console.Write(prompt);
+
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine(); // Move to the next line after Enter is pressed
+                break;
+            }
+            else if (keyInfo.Key == ConsoleKey.Backspace && inputBuilder.Length > 0)
+            {
+                inputBuilder.Remove(inputBuilder.Length - 1, 1);
+                Console.Write("\b \b"); // Move the cursor back, erase the character, and move the cursor back again
+            }
+            else if (!char.IsControl(keyInfo.KeyChar))
+            {
+                inputBuilder.Append(keyInfo.KeyChar);
+                Console.Write(keyInfo.KeyChar);
+            }
+        }
+
+        return inputBuilder.ToString();
+    }
+    }
+
+    namespace User
+{
+    class User_Command
+    {
+        public static void Input(string command)
+        {
+            switch (command.ToLower())
+            {
+                case "exit":
+                Console.Beep();
+                break;
+                default:
+                Console.WriteLine("Reload");
+                Program.Main();
+                break;
+            }
+        }
+    }
+}
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Text;
-using User_Commands;
+using Command.User;
 using Startup;
+using Command;
 
 namespace TextAdventure
 {
@@ -10,44 +11,19 @@ namespace TextAdventure
         public static void Main()
         {
             App.Load();
-        App.OnStart();
-        Console.WriteLine("Hello World");
-        User_Command.Input(Command());
-    }
-
-    private static string Command()
-    {
-        string input = CustomConsoleInput("➜ ");
-        return input ?? "";
-    }
-
-    static string CustomConsoleInput(string prompt)
-    {
-        StringBuilder inputBuilder = new StringBuilder();
-        Console.Write(prompt);
-
-        while (true)
-        {
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-            if (keyInfo.Key == ConsoleKey.Enter)
-            {
-                Console.WriteLine(); // Move to the next line after Enter is pressed
-                break;
-            }
-            else if (keyInfo.Key == ConsoleKey.Backspace && inputBuilder.Length > 0)
-            {
-                inputBuilder.Remove(inputBuilder.Length - 1, 1);
-                Console.Write("\b \b"); // Move the cursor back, erase the character, and move the cursor back again
-            }
-            else if (!char.IsControl(keyInfo.KeyChar))
-            {
-                inputBuilder.Append(keyInfo.KeyChar);
-                Console.Write(keyInfo.KeyChar);
-            }
+            App.OnStart();
+            Console.WriteLine("Hello World");
+            User_Command.Input(Commanddd());
+            Command.Command.CustomConsoleInput("");
         }
 
-        return inputBuilder.ToString();
+    private static string Commanddd()
+    {
+        // string input = CustomConsoleInput("➜ ");
+        // return input ?? "";
+        return "";
     }
     }
 }
+
+//Fix the Layout and Fix the Command Module
