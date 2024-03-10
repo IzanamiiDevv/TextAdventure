c3471c308c50d554b708de0c0d2d7a6ac9f2cb5c
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-10 11:28:15 +0800

    ss

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using User_Commands;
 using Startup;
 
@@ -16,7 +17,37 @@ namespace TextAdventure
 
     private static string Command()
     {
-        return Console.ReadLine() ?? "";
+        string input = CustomConsoleInput("➜ ");
+        return input ?? "";
     }
-}
+
+    static string CustomConsoleInput(string prompt)
+    {
+        StringBuilder inputBuilder = new StringBuilder();
+        Console.Write(prompt);
+
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine(); // Move to the next line after Enter is pressed
+                break;
+            }
+            else if (keyInfo.Key == ConsoleKey.Backspace && inputBuilder.Length > 0)
+            {
+                inputBuilder.Remove(inputBuilder.Length - 1, 1);
+                Console.Write("\b \b"); // Move the cursor back, erase the character, and move the cursor back again
+            }
+            else if (!char.IsControl(keyInfo.KeyChar))
+            {
+                inputBuilder.Append(keyInfo.KeyChar);
+                Console.Write(keyInfo.KeyChar);
+            }
+        }
+
+        return inputBuilder.ToString();
+    }
+    }
 }
